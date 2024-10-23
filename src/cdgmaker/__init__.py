5cd1da04065f2f94ff56f9736d7d813bddce714b
Andrew Beveridge <andrew@beveridge.uk>
2024-10-22 23:40:42 -0500

    Removed unnecessary comments

--- src/cdgmaker/__init__.py
+++
@@ -1,6 +0,0 @@
-# We'll import these later to avoid circular imports
-# from .composer import KaraokeComposer
-# from .config import Settings
-
-# If you need to expose any package-level constants or functions, you can do so here
-
